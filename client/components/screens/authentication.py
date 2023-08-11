4b0e6acf85d1e542f784acf041ebd816d2311c72
Winter_Snake <blizzardsnake98@gmail.com>
2023-08-11 19:13:54 -0400

    add: socket

--- client/components/screens/authentication.py
+++ client/components/screens/authentication.py
@@ -7,10 +7,47 @@
 ##-------------------------------##
 
 ## Imports
+import socket
+
 from kivy.uix.screenmanager import Screen
 
 
 ## Classes
 class AuthenticationScreen(Screen):
     """"""
-    pass
+
+    # -Constructor
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self._socket: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # -Instance Methods: Event
+    def on_login(self) -> None:
+        print("Login")
+        if not self._connect():
+            print("error connecting...")
+            return
+
+    def on_register(self) -> None:
+        print("Register")
+        if not self._connect():
+            print("error connecting...")
+            return
+
+    # -Instance Methods: Private
+    def _connect(self) -> bool:
+        print(self.address)
+
+        try:
+            self._socket.connect(self.address)
+        except ConnectionRefusedError:
+            return False
+        return True
+
+    # -Properties
+    @property
+    def address(self) -> tuple[str, int]:
+        return (
+            self.ids['ip'].text,
+            int(self.ids['port'].text)
+        )
